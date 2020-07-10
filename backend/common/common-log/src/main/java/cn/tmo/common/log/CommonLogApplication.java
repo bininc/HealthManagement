9b6e3750e0542e32687bafa5250ee1c3b939a839
bininc <binbin.yan@linkedcare.cn>
2020-07-10 19:01:50 +0800

    添加后端JAVA模块儿

---
+++ backend/common/common-log/src/main/java/cn/tmo/common/log/CommonLogApplication.java
@@ -0,0 +1,13 @@
+package cn.tmo.common.log;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommonLogApplication.class, args);
+    }
+
+}
---
+++ backend/common/common-log/src/test/java/cn/tmo/common/log/CommonLogApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.tmo.common.log;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonLogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
