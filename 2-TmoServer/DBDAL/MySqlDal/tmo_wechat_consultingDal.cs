86f0d3cce40815bf7e03d4ebf1ffd8887525e7f2
Bininc <bininc@163.com>
2020-12-05 21:13:44 +0800

    优化引用

--- 2-TmoServer/DBDAL/MySqlDal/tmo_wechat_consultingDal.cs
+++ 2-TmoServer/DBDAL/MySqlDal/tmo_wechat_consultingDal.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using DBInterface;
 using TmoCommon;
-using MySql.Data.MySqlClient;
 using DBUtility.MySQL;
-using System;
 using System.Data;
 
 namespace DBDAL.MySqlDal
